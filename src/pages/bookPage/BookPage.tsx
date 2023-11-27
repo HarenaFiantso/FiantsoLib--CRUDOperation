67e844c62dbc657bbc56411491b13460c0acc17d
HarenaFiantso <fiantsorav@gmail.com>
2023-11-27 17:23:53 +0300

    style: Create the main layout of the book page

--- src/pages/bookPage/BookPage.tsx
+++ src/pages/bookPage/BookPage.tsx
@@ -1,10 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Book from '../../type/BookType';
+import axios from 'axios';
 
 const BookPage: React.FC = () => {
-  const [books, setBooks] = useState([]);
-  return <>
-    <h1>Welcome to <span>FiantsoLib</span></h1>
-  </>;
-}
+  const [books, setBooks] = useState<Book[]>([]);
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/book');
+        setBooks(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+  console.log(books);
+
+  return (
+    <>
+      <h1>
+        Welcome to <span>FiantsoLib</span>
+      </h1>
+      <div className="books">
+        {books.map((book) => (
+          <div key={book.id} className="book">
+            <img src={book.cover} alt="book cover" />
+            <h2>{book.title}</h2>
+            <p>{book.description}</p>
+            <span>$ {book.price}</span>
+            <button className="delete">Delete</button>
+            <button className="update">Update</button>
+          </div>
+        ))}
+      </div>
+
+      <button className="addBook">Add new book</button>
+    </>
+  );
+};
 
 export default BookPage;
