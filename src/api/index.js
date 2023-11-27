485eb2fbb60a7fff29227d272e4c184b6ede6bc1
HarenaFiantso <fiantsorav@gmail.com>
2023-11-27 16:03:29 +0300

    chore: Implement the GET /book/:id enpoint who receive book by his id

--- src/api/index.js
+++ src/api/index.js
@@ -16,6 +16,46 @@ app.get('/book', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+app.get('/book/:id', async (req, res) => {
+  const bookId = parseInt(req.params.id);
+  try {
+    const { rows } = await db.query('SELECT * FROM book WHERE id = $1', [
+      bookId,
+    ]);
+    if (rows.length === 0) {
+      return res.status(404).send('Book not found');
+    }
+    res.json(rows[0]);
+  } catch (error) {
+    res.status(500).send('Error while receiving book');
+  }
+});
+app.put('/book/:id', async (req, res) => {
+  const bookId = parseInt(req.params.id);
+  const { title, description, price, cover } = req.body;
+
+  try {
+    const updateQuery = `
+        UPDATE book 
+        SET title = $1, description = $2, price = $3, cover = $4 
+        WHERE id = $5
+        RETURNING *
+      `;
+    const { rows } = await db.query(updateQuery, [
+      title,
+      description,
+      price,
+      cover,
+      bookId,
+    ]);
+    if (rows.length === 0) {
+      return res.status(404).send('Book not found');
+    }
+    res.json(rows[0]);
+  } catch (error) {
+    res.status(500).send('Erreur lors de la mise à jour du livre');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Backend server is running on port ${PORT}`);
