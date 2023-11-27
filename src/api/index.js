a34f989d59dd2290d55b3bed7a8677dd5b83115b
HarenaFiantso <fiantsorav@gmail.com>
2023-11-27 15:48:18 +0300

    chore: Configure the database connection with the backend server

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,20 @@
-import express from 'express';
-import dotenv from 'dotenv';
+const express = require('express');
+const dotenv = require('dotenv');
+const db = require('./db');
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
 /* Controller */
-app.get('/', function (req, res) {
-  res.send('Hello World');
+app.get('/book', async (req, res) => {
+  try {
+    const result = await db.query('SELECT * FROM book');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.listen(PORT, () => {
