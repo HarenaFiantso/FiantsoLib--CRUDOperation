cf423d83e4d834dbf9e356470cf7821e5540d32f
HarenaFiantso <fiantsorav@gmail.com>
2023-11-27 16:50:31 +0300

    fix: Confiure react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BookPage from './pages/bookPage/BookPage';
+import AddBookPage from './pages/addBookPage/AddBookPage';
+import UpdateBookPage from './pages/updateBookPage/UpdateBookPage';
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BookPage />}></Route>
+          <Route path="/add" element={<AddBookPage />}></Route>
+          <Route path="/update/:id" element={<UpdateBookPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/updateBookPage/UpdateBookPage.tsx
@@ -0,0 +1,3 @@
+export default function UpdateBookPage() {
+  return <div>UpdateBookPage</div>;
+}
